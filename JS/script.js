66dfb64f9b3ac3b29721afcef11959e9510716e3
Pedro <pedrod33@ua.pt>
2022-11-14 20:09:55 +0000

    load_dataset

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,18 @@
 //function that is called on the load of the body, and receives the current page
-function main(page) {
+async function main(page) {
   //Menu appears in all pages, so we render it, and pass the current page, so it know which tab is active
   renderMenu(page);
 
+  //load datest
+  const data = await d3.dsv(";", "2022-2023_NBA_Player_Stats.csv", (d) => {
+    return d;
+  });
+
+  
+  //how to get information from dataset
+  console.log(data[0]["Player"])
+  
+  
   //switch with all pages
   switch (page) {
     case "players":
