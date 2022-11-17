d117c7df6d6f7f23423db13e5cfa522e8c013a10
ricardocruz29 <ricardo.cruz29@ua.pt>
2022-11-17 01:12:56 +0000

    Added general info for one player. Not styled

--- JS/player.js
+++ JS/player.js
@@ -3,5 +3,32 @@ function addGeneralInfoPlayer(
   player2 = undefined,
   player3 = undefined
 ) {
-  console.log("player1", player1);
+  const main = document.getElementById("player_main");
+  const listPlayerAttributes = document.getElementById("player_attributes");
+
+  const header = document.createElement("h1");
+  header.textContent = `Player: ${player1.Player}`;
+
+  //Create elements with general info
+  const playerTeam = document.createElement("span");
+  playerTeam.textContent = getTeam(player1.Tm).name;
+  const playerAge = document.createElement("span");
+  playerAge.textContent = player1.Age;
+  const playerPosition = document.createElement("span");
+  playerPosition.textContent = player1.Pos;
+
+  //get List elements
+  const playerTeamEl = document.getElementById("player_team");
+  const playerPositionEl = document.getElementById("player_position");
+  const playerAgeEl = document.getElementById("player_age");
+
+  //add general info to list elements
+  playerTeamEl.appendChild(playerTeam);
+  playerPositionEl.appendChild(playerPosition);
+  playerAgeEl.appendChild(playerAge);
+
+  //add href to link
+  playerTeamEl.href = `./team.html?id=${player1.Tm}`;
+
+  main.insertBefore(header, listPlayerAttributes);
 }
