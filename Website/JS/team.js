b629016710d329e381587206bc3c9012c5531c22
ricardocruz29 <ricardo.cruz29@ua.pt>
2022-12-10 12:09:55 +0000

    Created folder for website

---
+++ Website/JS/team.js
@@ -0,0 +1,70 @@
+function showTeamHeader(teamId, players) {
+  const team_main = document.getElementById("team_main");
+  let teamInfo = getTeam(teamId);
+
+  const teamInfoDiv = document.getElementById("teamInfoDiv");
+  const team_name = document.createElement("h1");
+  team_name.textContent = teamInfo.name;
+  teamInfoDiv.appendChild(team_name);
+  const teamImg = document.createElement("img");
+  teamImg.src = teamInfo.image;
+  teamImg.alt = `Logo of team ${teamInfo.name}`;
+  teamImg.id = "team_logo";
+  teamInfoDiv.appendChild(teamImg);
+
+  const line_graph_div = document.getElementById("team_line_graph_id");
+
+  const player_tooltip = d3
+    .select("#team_line_graph_id")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
+    .style("position", "absolute");
+
+  let team_data = individualTeamData(players);
+  let statKeys = [
+    "Age",
+    "G",
+    "MP",
+    "2P",
+    "3P",
+    "AST",
+    "BLK",
+    "FG",
+    "FT",
+    "STL",
+    "TOV",
+    "TRB",
+  ];
+  drawLineGraph(
+    team_data,
+    statKeys,
+    line_graph_div,
+    player_tooltip,
+    "Individual Stats"
+  );
+}
+
+function showTeamStats(players) {
+  let def_stats = { BLK: {}, STL: {}, TRB: {}, PF: {} };
+  let off_stats = { "2P": {}, "3P": {}, AST: {}, FT: {} };
+  let team_data = individualTeamData(players);
+  for (let i in Object.keys(team_data)) {
+    team_data[Object.keys(team_data)[i]].sort(function (a, b) {
+      return a - b;
+    });
+  }
+
+  const off_div = document.getElementById("boxPlotAttackTeam");
+  const def_div = document.getElementById("boxPlotDefenseTeam");
+
+  drawBoxPlot(off_stats, team_data, off_div,["2 Point Goals","3 Point Goals", "Assists per game","Free Throw Goals"]
+  );
+  drawBoxPlot(def_stats, team_data, def_div,["Blocks per game","Steals per game", "Rebounds per game","Fouls per Game"]
+  );
+}
---
+++ Website/data/playersData.js
@@ -0,0 +1,99 @@
+function getPlayerStats(player, allPlayers) {
+  // To convert from 3ps% to
+  // outsideScoring rating (0-100),
+  // We do an optimistic normalization that subtracts the average to the max, so the values are generally higher
+  const threePts = allPlayers.map((p) => +p["3P%"]);
+  const threePtsAverage = threePts.reduce((a, b) => a + b, 0) / threePts.length;
+  const outsideScoring =
+    normalize(+player["3P%"], threePts, threePtsAverage / 2) * 100;
+
+  // To convert from 2ps% to
+  // insideScoring rating (0-100),
+  // We do an optimistic normalization that subtracts the average to the max, so the values are generally higher
+  const twoPts = allPlayers.map((p) => +p["2P%"]);
+  const twoPtsAverage = twoPts.reduce((a, b) => a + b, 0) / twoPts.length;
+  const insideScoring =
+    normalize(+player["2P%"], twoPts, twoPtsAverage / 2) * 100;
+
+  // To convert from ft% to
+  // freeThrow rating (0-100),
+  // We do an optimistic normalization that subtracts the average to the max, so the values are generally higher
+  const freeThrows = allPlayers.map((p) => +p["FT%"]);
+  console.log("freeThrows", freeThrows);
+  const freeThrowAverage =
+    freeThrows.reduce((a, b) => a + b, 0) / freeThrows.length;
+  console.log("freeThrowsAverage", freeThrowAverage);
+  console.log("freeThrow", player["FT%"]);
+  const freeThrow =
+    normalize(+player["FT%"], freeThrows, freeThrowAverage / 4) * 100;
+
+  // To convert from number of assists per game to
+  // Playmaking rating (0-100),
+  // We do an optimistic normalization that subtracts the average to the max, so the values are generally higher
+  const assists = allPlayers.map((p) => +p.AST);
+  const assistsAverage = assists.reduce((a, b) => a + b, 0) / assists.length;
+  const playmaking = normalize(+player.AST, assists, assistsAverage / 2) * 100;
+
+  // To convert from number of rebounds per game (offensive + defensive) to
+  // rebounding rating (0-100),
+  // We do an optimistic normalization that subtracts the average to the max, so the values are generally higher
+  const rebounds = allPlayers.map((p) => +p.ORB + +p.DRB);
+  const reboundsAverage = rebounds.reduce((a, b) => a + b, 0) / rebounds.length;
+  const playerRebounding = +player.ORB + +player.DRB;
+  const rebounding =
+    normalize(playerRebounding, rebounds, reboundsAverage / 2) * 100;
+
+  // To convert from number of steals and blocks per game to
+  // Defending rating (0-100),
+  // We do an optimistic normalization that subtracts the average to the max, so the values are generally higher
+  const blockingStats = allPlayers.map((p) => +p.BLK);
+  const blockingStatsAverage =
+    blockingStats.reduce((a, b) => a + b, 0) / blockingStats.length;
+  const playerBlockingStats = +player.BLK;
+  const blocking =
+    normalize(playerBlockingStats, blockingStats, blockingStatsAverage / 2) *
+    100;
+
+  // To convert from number of steals and blocks per game to
+  // Defending rating (0-100),
+  // We do an optimistic normalization that subtracts the average to the max, so the values are generally higher
+  const stealStats = allPlayers.map((p) => +p.STL);
+  const stealStatsAverage =
+    stealStats.reduce((a, b) => a + b, 0) / stealStats.length;
+  const playerStealStats = +player.STL;
+  const stealing =
+    normalize(playerStealStats, stealStats, stealStatsAverage / 2) * 100;
+
+  const stats = {
+    outsideScoring: round(outsideScoring, 0),
+    insideScoring: round(insideScoring, 0),
+    freeThrow: round(freeThrow, 0),
+    playmaking: round(playmaking, 0),
+    rebounding: round(rebounding, 0),
+    blocking: round(blocking, 0),
+    stealing: round(stealing, 0),
+  };
+
+  function normalize(val, allVal, extraMax = 0) {
+    //extraMax is to make the normalization get higher values to ignore values to close to 0
+    const min = Math.min(...allVal);
+    let max = Math.max(...allVal) - extraMax;
+
+    //Since we are adding an extra, it can turn it to less than 0, so we have to have a fallback
+    if (max - min <= 0) max = Math.max(...allVal);
+
+    let normalizedVal = (val - min) / (max - min);
+
+    //Since we are adding an extra, it can turn it to greater than 1, so we have to have a fallback
+    if (normalizedVal > 1) normalizedVal = 1;
+
+    return normalizedVal;
+  }
+
+  function round(value, precision) {
+    const multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+  }
+
+  return stats;
+}
